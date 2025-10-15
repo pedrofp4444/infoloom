83a07f5a1017c0ce6cb4e788aa4f242e56ddc6d3
Pedro Pereira <pfpereira2004@gmail.com>
2025-10-15 21:05:10 +0100

    chore: add SSR adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,15 @@ import vue from "@astrojs/vue";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [vue()],
+  integrations: [vue()],
 
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  adapter: vercel(),
 });
