0efd5edac3d2c6736f9ee5ae3d82e0493accbd4a
Pedro Pereira <pfpereira2004@gmail.com>
2025-10-27 16:57:27 +0000

    feat: improve API

--- src/pages/api/ucs.ts
+++ src/pages/api/ucs.ts
@@ -1,20 +1,8 @@
-import fs from "fs";
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async () => {
-    try {
-        const data = fs.readFileSync(
-            new URL("../../../public/data/ucs.json", import.meta.url),
-            "utf-8",
-        );
-        return new Response(data, {
-            status: 200,
-            headers: { "Content-Type": "application/json" },
-        });
-    } catch (err) {
-        return new Response(
-            JSON.stringify({ error: "Could not read the file." }),
-            { status: 500 },
-        );
-    }
+    return new Response(null, {
+        status: 307,
+        headers: { Location: "/data/ucs.json" },
+    });
 };
